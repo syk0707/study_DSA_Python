63df7922d2582d56181c92b66ccdaa81e07f6b1d
김상윤 <sykim@miksystem.net>
2021-03-15 22:58:33 +0900

    prime num, bubble sort commit

--- algorithm/sort.py
+++ algorithm/sort.py
@@ -1,3 +1,5 @@
+import random
+
 def array_num():
     total_num = int(input())
     total_obj = {}
@@ -11,5 +13,20 @@ def array_num():
     for print_num in total_arr:
         print(print_num)
 
+
+def bubble_sort():
+    data_list = random.sample(range(100), 50)
+    print(data_list)
+    for idx in range(len(data_list) - 1):
+        swap = False
+        for idx2 in range(len(data_list) - idx - 1):
+            if data_list[idx2] > data_list[idx2 + 1]:
+                data_list[idx2], data_list[idx2 + 1] = data_list[idx2 + 1], data_list[idx2]
+                swap = True
+        if swap is False:
+            break
+    print(data_list)
+
 if __name__ == "__main__":
-    array_num()
+    # array_num()
+    bubble_sort()
