5065d3bc4007803f5ce85f62027e256c12eb16c3
SYKim <syk0707@naver.com>
2021-03-20 08:22:31 +0900

    modify sort

--- algorithm/sort.py
+++ algorithm/sort.py
@@ -1,3 +1,6 @@
+import sys
+import collections
+
 def array_num():
     total_num = int(input())
     total_obj = {}
@@ -11,5 +14,18 @@ def array_num():
     for print_num in total_arr:
         print(print_num)
 
+def array_num_2():
+    total_num = int(sys.stdin.readline())
+    total_deque = deque()
+    for total_idx in range(total_num):
+        num = int(sys.stdin.readline())
+        total_arr.append(num)
+    nl = "\n"
+    while total_deque:
+        sys.stdout.write(f"{total_arr[reverse_idx]}{nl}")
+
 if __name__ == "__main__":
-    array_num()
+    #array_num()
+    #array_num_2()
+    print(chr(74))
+    print(ord("C"))
