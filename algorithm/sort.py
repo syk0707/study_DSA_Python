bc1c8ed907c4ed1a8805307250004c3b3cdc2554
김상윤 <sykim@miksystem.net>
2021-03-21 16:17:21 +0900

    modify sort

--- algorithm/sort.py
+++ algorithm/sort.py
@@ -1,4 +1,5 @@
 import random
+import sys
 
 
 def array_num():
@@ -41,7 +42,82 @@ def insertion_sort():
     print(data_list)
 
 
+def quick_sort_example(data):
+    if len(data) <= 1:
+        return data
+
+    pivot = data[0]
+
+    # left, right = [], []
+    #
+    # for idx in range(1, len(data)):
+    #     if pivot > data[idx]:
+    #         left.append(data[idx])
+    #     else:
+    #         right.append(data[idx])
+
+    left = [item for item in data[1:] if pivot > item]
+    right = [item for item in data[1:] if pivot <= item]
+
+    return quick_sort_example(left) + [pivot] + quick_sort_example(right)
+
+
+# def quick_sort_recursive(sort_arr):
+#     if len(sort_arr) <= 1:
+#         return sort_arr
+#     pivot = sort_arr[0]
+#
+#     left_arr, right_arr = [], []
+#
+#     for idx in range(1, len(sort_arr)):
+#         if pivot > sort_arr[idx]:
+#             left_arr.append(sort_arr[idx])
+#         else:
+#             right_arr.append(sort_arr[idx])
+#
+#     return quick_sort_recursive(left_arr) + [pivot] + quick_sort_recursive(right_arr)
+def quick_sort_recursive(sort_arr):
+    if len(sort_arr) <= 1:
+        return sort_arr
+    pivot = sort_arr[0]
+    left_arr = [item for item in sort_arr[1:] if pivot > item]
+    right_arr = [item for item in sort_arr[1:] if pivot <= item]
+
+    return quick_sort_recursive(left_arr) + [pivot] + quick_sort_recursive(right_arr)
+
+
+def quick_sort_2752():
+    total_arr = list(map(int, sys.stdin.readline().split()))
+    ret_arr = quick_sort_recursive(total_arr)
+    for ret_idx in ret_arr:
+        sys.stdout.write(str(ret_idx) + " ")
+
+
+def quick_sort_1427():
+    total_num = sys.stdin.readline().rstrip('\n')
+    list_arr = [int(num) for num in total_num]
+    ret_arr = quick_sort_recursive(list_arr)
+    for ret_idx in range(len(ret_arr) - 1, -1, -1):
+        sys.stdout.write(str(ret_arr[ret_idx]))
+
+
+def split(data):
+    medium = int(len(data) / 2)
+    left = data[:medium]
+    right = data[medium:]
+    print(left, right)
+
+
+def merge_sort():
+    print('merge')
+
+
 if __name__ == "__main__":
     # array_num()
     # bubble_sort()
-    insertion_sort()
+    # insertion_sort()
+    random_list = random.sample(range(100), 10)
+    # print(quick_sort_example(random_list))
+    # quick_sort_2752()
+    # quick_sort_1427()
+    split(random_list)
