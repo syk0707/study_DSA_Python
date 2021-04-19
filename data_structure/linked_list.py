e05bd98e92ca9dab348dae8350e6430234b2945f
SYKim <syk0707@naver.com>
2021-04-19 23:24:46 +0900

    add list

--- data_structure/linked_list.py
+++ data_structure/linked_list.py
@@ -1,3 +1,6 @@
+import sys
+
+
 class Node:
     def __init__(self, data, prev=None, next=None):
         self.prev = prev
@@ -62,12 +65,31 @@ class NodeMgmt:
             return True
 
 
+def list_2920():
+    num_list = list(map(int, sys.stdin.readline().split()))
+    is_asc = True
+    is_mixed = False
+    if num_list[0] > num_list[1]:
+        is_asc = False
+    for num in range(1, len(num_list) - 1):
+        if is_asc is True and num_list[num] >= num_list[num + 1] or is_asc is False and num_list[num] <= num_list[num + 1]:
+            is_mixed = True
+            break
+    if is_mixed is True:
+        print("mixed")
+    elif is_asc is True:
+        print("ascending")
+    else:
+        print("descending")
+
+
 if __name__ == "__main__":
-    double_linked_list = NodeMgmt(0)
-    for data in range(1, 10):
-        double_linked_list.insert(data)
-    double_linked_list.desc()
-    node_3 = double_linked_list.search_from_head(7)
-    print(node_3.data)
-    node_before = double_linked_list.insert_before(1.5, 2)
-    double_linked_list.desc()
+    # double_linked_list = NodeMgmt(0)
+    # for data in range(1, 10):
+    #     double_linked_list.insert(data)
+    # double_linked_list.desc()
+    # node_3 = double_linked_list.search_from_head(7)
+    # print(node_3.data)
+    # node_before = double_linked_list.insert_before(1.5, 2)
+    # double_linked_list.desc()
+    list_2920()
