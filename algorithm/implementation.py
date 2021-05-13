18637f6beb9a6fa5370301d40cd09f15614491e2
SYKim <syk0707@naver.com>
2021-05-13 23:07:56 +0900

    add implementation 14912

--- algorithm/implementation.py
+++ algorithm/implementation.py
@@ -8,5 +8,16 @@ def rectangles_15232():
         sys.stdout.write(f"{'*' * c}\n")
 
 
+def freq_num_14912():
+    num_arr = list(map(int, sys.stdin.readline().split()))
+    tot = 0
+    for idx in range(1, num_arr[0] + 1):
+        for num_str in str(idx):
+            if num_str == str(num_arr[1]):
+                tot += 1
+    sys.stdout.write(f"{tot}")
+
+
 if __name__ == '__main__':
-    rectangles_15232()
+    # rectangles_15232()
+    freq_num_14912()
