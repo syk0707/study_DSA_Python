c031d0e4ae29c680fcafb717baa3b097d8ed0d3d
SYKim <syk0707@naver.com>
2021-07-24 18:31:29 +0900

    add two pointer 1920

--- algorithm/two_pointer.py
+++ algorithm/two_pointer.py
@@ -1,3 +1,4 @@
+import time
 import sys
 
 
@@ -9,5 +10,18 @@ def join_arr_11728():
     sys.stdout.write(f"{' '.join(str(i) for i in sorted(first_arr))}")
 
 
+def find_arr_1920():
+    tot_n = int(sys.stdin.readline())
+    tot_dic = set(sys.stdin.readline().split())
+    case_m = int(sys.stdin.readline())
+    case_arr = sys.stdin.readline().split()
+    for case_num in case_arr:
+        if case_num in tot_dic:
+            sys.stdout.write("1\n")
+        else:
+            sys.stdout.write("0\n")
+
+
 if __name__ == "__main__":
-    join_arr_11728()
+    # join_arr_11728()
+    find_arr_1920()
