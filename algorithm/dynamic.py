f37befe3e39919d0f946b314222a407061a11ab5
SYKim <syk0707@naver.com>
2021-07-25 10:07:19 +0900

    add dynamic 1003

--- algorithm/dynamic.py
+++ algorithm/dynamic.py
@@ -14,7 +14,6 @@ def dynamic_fibonacci(num):
     cache = [0 for idx in range(num + 1)]
     cache[0] = 0
     cache[1] = 1
-
     for idx in range(2, num + 1):
         cache[idx] = cache[idx - 1] + cache[idx - 2]
     return cache[num]
@@ -44,8 +43,23 @@ def fibonacci_9625():
     print(f"{arr[num - 1]} {arr[num]}")
 
 
+def fibonacci_1003():
+    tot_num = int(sys.stdin.readline())
+    for idx in range(tot_num):
+        case_num = int(sys.stdin.readline())
+        if case_num == 0:
+            sys.stdout.write("1 0\n")
+            continue
+        elif case_num == 1:
+            sys.stdout.write("0 1\n")
+            continue
+        else:
+            sys.stdout.write(f"{dynamic_fibonacci(case_num - 1)} {dynamic_fibonacci(case_num)}\n")
+
+
 if __name__ == "__main__":
     # print(recursive_fibonacci(4))
     # print(dynamic_fibonacci(5))
     # print(fibonacci_2748())
-    fibonacci_9625()
+    # fibonacci_9625()
+    fibonacci_1003()
